3e0cbdee7b7fc46ef18fc800b0041d16d2ba4167
Admin <za0557165@gmail.com>
2026-04-13 17:48:49 +0500

    Add root route redirect to student dashboard

--- home/urls.py
+++ home/urls.py
@@ -2,14 +2,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect  # Add this import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('/student-dashboard/')),  # Add this line - redirects to student dashboard
     path('', include("deptts.urls")),
     path('student', include("student.urls")),
     path('teachers', include("teachers.urls")),
     path('subject', include("subject.urls")),
-     path('ddcomit', include("ddcomit.urls")),
+    path('ddcomit', include("ddcomit.urls")),
     path('home_auth', include("home_auth.urls")),
     path('head', include("head.urls")),
     path('fee_system', include("fee_system.urls")),
@@ -20,24 +22,4 @@ urlpatterns = [
     path('token_app', include("token_app.urls")),
     path('timetables', include("timetables.urls")),
     path('assignm', include("assignm.urls")),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    
